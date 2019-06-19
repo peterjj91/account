c2b249ffbbeef70c09895a67b7c5a62f43e734c3
Popeskul Pavel <peterjj91@gmail.com>
2019-06-19 14:42:01 +0300

    index desktop

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   
 });
+
+
+$(document).ready(function() {
+  $('.slider').slick({
+    infinite: false,
+    dots: true,
+    appendDots: $(".slider__dots"),
+    prevArrow: $('.slider__arrow--prev'),
+    nextArrow: $('.slider__arrow--next')
+  });
+});
