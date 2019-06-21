af8ab625709f6b06a1ec757978c5206a70c7b933
Popeskul Pavel <peterjj91@gmail.com>
2019-06-21 16:04:51 +0300

    index mobile

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -12,3 +12,41 @@ $(document).ready(function() {
     nextArrow: $('.slider__arrow--next')
   });
 });
+
+$(document).ready(function(){
+  $('.hamburger').click(function(){
+    // $(this).toggleClass('open');
+    // slideout.close();
+    slideout.toggle();
+  });
+});
+
+// MAIN MENU
+var slideout = new Slideout({
+  'panel': document.getElementById('main'),
+  'menu': document.getElementById('menu'),
+  'side': 'right',
+  'padding': 320,
+  'tolerance': 70
+});
+
+document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
+  slideout.toggle();
+});
+
+function close(eve) {
+  eve.preventDefault();
+  slideout.close();
+}
+
+slideout
+  .on('beforeopen', function() {
+    this.panel.classList.add('panel-open');
+  })
+  .on('open', function() {
+    this.panel.addEventListener('click', close);
+  })
+  .on('beforeclose', function() {
+    this.panel.classList.remove('panel-open');
+    this.panel.removeEventListener('click', close);
+  });
