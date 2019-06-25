a12ee121fc3e269f6f35cc29b735f2e813eb3dc1
Popeskul Pavel <peterjj91@gmail.com>
2019-06-25 18:05:25 +0300

    all pages

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -61,9 +61,9 @@ slideout
 
 
 // range slider
-var rangeSlider = document.getElementById('slider-range');
+if (document.getElementById('slider-range')) {
+  var rangeSlider = document.getElementById('slider-range');
 
-// $(document).ready(function() {
   noUiSlider.create(rangeSlider, {
     start: [4000],
     connect: 'lower',
@@ -76,11 +76,57 @@ var rangeSlider = document.getElementById('slider-range');
         suffix: ' рублей'
     })
   });
-// });
 
-// Display the slider value
-var rangeSliderValueElement = document.getElementById('slider-range-value');
+  // Display the slider value
+  var rangeSliderValueElement = document.getElementById('slider-range-value');
 
-rangeSlider.noUiSlider.on('update', function (values, handle) {
-    rangeSliderValueElement.innerHTML = values[handle];
+  rangeSlider.noUiSlider.on('update', function (values, handle) {
+      rangeSliderValueElement.innerHTML = values[handle];
+  });
+}
+
+
+// read more
+var bindReadMore = function(){
+  $('.read-more').on('click', function(e) {
+    e.preventDefault();
+    var parent = $(this).parent();
+    parent.trigger("destroy");
+    parent.removeClass('truncable-txt--is-truncated');
+    parent.addClass('truncable-txt--is-not-truncated');
+    bindReadLess(); // bind click on "less"
+  });
+};
+
+var bindReadLess = function(){
+  $('.read-less').on('click', function(e) {
+    e.preventDefault();
+    var parent = $(this).parent();
+    truncateIfNeeded(parent); // Re-initialize ellipsis
+  });
+};
+
+var truncateIfNeeded = function(jqueryTag){
+  var $selectionToTruncate = jqueryTag || $('.truncable-txt');
+  
+  $selectionToTruncate.dotdotdot({
+    ellipsis: '...',
+    watch   : true,
+    wrap    : 'letter',
+    height  : 200, // max number of lines
+    after   : '.read-more',
+    callback: function( isTruncated, orgContent ) {
+      var $currentReadMore = $(this).find('.read-more');
+      var $currentReadLess = $(this).find('.read-less');
+      
+      if( isTruncated ){
+        $(this).addClass('truncable-txt--is-truncated');
+      }
+      bindReadMore(); // bind click on "read more"
+    },
+  });
+};
+
+$(function() {
+  truncateIfNeeded(); // Initialize ellipsis
 });
